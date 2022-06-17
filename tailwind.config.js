818f636a1e0a86b7e3ff5ce9852bafc3f5217da2
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-17 16:35:40 +0200

    Changing fonts and cleaning views

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
+    './app/assets/stylesheets/fonts/*.*',
     './app/javascript/**/*.js',
     './src/**/*.{html,js}',
     './node_modules/tw-elements/dist/js/**/*.js'
